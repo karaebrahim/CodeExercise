15094f3baf7e2ee6948633eca954040e38451e0d
Kara Ebrahim <kebrahim@hubspot.com>
2024-03-09 13:20:49 -0500

    Responsive design

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,14 +1,55 @@
 import React from 'react';
 import styled from 'styled-components';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Wrapper = styled.div`
   display: flex;
+  width: 100%;
+  flex-grow: 1;
+  flex-direction: column;
+  margin-bottom: 1rem;
+
+  img {
+    height: 100%;
+    object-fit: cover;
+  }
+
+  @media (min-width: 600px) {
+    width: 48%;
+  }
+
+  @media (min-width: 1080px) {
+    width: 25%;
+  }
 `;
 
-export default Card = (data) => {
+const Title = styled.h2`
+  margin: 1rem 0;
+  font-size: 1.25rem;
+`;
+
+const Para = styled.p`
+  margin: 0;
+`;
+
+const addDefaultSrc = (event) => {
+  event.target.src = 'https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_640.jpg';
+}
+
+export default Card = ({ data }) => {
   return (
     <Wrapper>
-      <h2>{data.media.title}</h2>
+      <LazyLoadImage src={data.poster} alt={data.title} onError={addDefaultSrc} />
+			<Title>{data.title} {data.year && <span>({data.year})</span>}</Title>
+			{data.genre &&
+				<Para>
+					<b>Genres: </b>
+					{data.genre
+						.map(a => a.charAt(0).toUpperCase() + a.substr(1))
+						.join(', ')
+					}
+				</Para>
+			}
     </Wrapper>
   )
 };
--- src/js/components/Media.js
+++ src/js/components/Media.js
@@ -1,58 +1,32 @@
 import React from 'react';
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
-import { LazyLoadImage } from "react-lazy-load-image-component";
+import Card from './Card';
 
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
-  width: 100%;
   gap: 1rem;
-`;
-
-const Card = styled.div`
-  display: flex;
-  width: 25%;
-  flex-grow: 1;
-  flex-direction: column;
-  margin-bottom: 1rem;
-
-  img {
-    height: 100%;
-    object-fit: cover;
-  }
-`;
-
-const Title = styled.h2`
-  margin: 1rem 0;
-  font-size: 1.25rem;
-`;
-
-const Para = styled.p`
-  margin: 0;
+  padding: 1rem;
 `;
 
 export default Media = () => {
   const [media, setMedia] = useState([]);
   useEffect(() => {
     const abortController = new AbortController()
-    fetch('http://localhost:3001/api/media', {signal: abortController.signal})
+    fetch('http://localhost:3001/api/media', { signal: abortController.signal })
       .then((res) => {
         return res.json();
       })
       .then((data) => {
         setMedia(data);
-      }).catch(err=>console.log(err))
-      return ()=>{abortController.abort() }
+      }).catch(err => console.log(err))
+      return () => { abortController.abort() }
   }, []);
   return (
     <Wrapper>
       {media.map((medium, i) => (
-        <Card key={i}>
-          <LazyLoadImage src={medium.poster} alt={medium.title} />
-          <Title>{medium.title} {medium.year && <span>({medium.year})</span>}</Title>
-          <Para>Genres: {medium.genre}</Para>
-        </Card>
+        <Card key={i} data={medium} />
       ))}
     </Wrapper>
   );
